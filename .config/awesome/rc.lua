dd2d387034b5ec55c41845b15ceabeb2bf70b22b
AmirKh <amirkh20@protonmail.com>
2021-06-24 13:30:33 +0430

    added Dynamic tagging from DT config

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -297,6 +297,19 @@ globalkeys = gears.table.join(
         end,
         {description = "go back", group = "client"}),
 
+-- Dynamic tagging
+    awful.key({ modkey, "Shift" }, "n", function () lain.util.add_tag() end,
+              {description = "add new tag", group = "tag"}),
+    awful.key({ modkey, "Shift" }, "r", function () lain.util.rename_tag() end,
+              {description = "rename tag", group = "tag"}),
+    awful.key({ modkey, "Shift" }, "Left", function () lain.util.move_tag(-1) end,
+              {description = "move tag to the left", group = "tag"}),
+    awful.key({ modkey, "Shift" }, "Right", function () lain.util.move_tag(1) end,
+              {description = "move tag to the right", group = "tag"}),
+    awful.key({ modkey, "Shift" }, "d", function () lain.util.delete_tag() end,
+              {description = "delete tag", group = "tag"}),
+
+
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
               {description = "open a terminal", group = "launcher"}),
