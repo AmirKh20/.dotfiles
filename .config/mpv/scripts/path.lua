2c9a0e150a7bd95e9caa7cbbb94dc3df0b97af04
AmirKh <amirkh20@protonmail.com>
2022-01-11 11:19:51 +0330

    path.lua now uses lua file handeling

--- .config/mpv/scripts/path.lua
+++ .config/mpv/scripts/path.lua
@@ -1,7 +1,9 @@
 -- write the working-directory to the /tmp/path_file for downloading and extracting subtitles from qutebrowser with subtitle script
 function get_working_directory()
     local path = mp.get_property("working-directory")
-    os.execute("echo '" .. path .. "' > " .. "/tmp/path_file")
+    file = io.open("/tmp/path_file", "w")
+    file:write(path)
+    file:close()
     mp.osd_message("path copied into /tmp/path_file", 10)
 end
 mp.add_key_binding('ctrl+s', 'get_working_directory', get_working_directory)
