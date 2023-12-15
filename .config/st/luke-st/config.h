9920d1a2b3f57f571a4b690396a8bbe965a346b7
AmirKh <amirkh20@protonmail.com>
2023-12-15 12:10:23 +0330

    changed sauce code pro nerd fonts names

--- .config/st/luke-st/config.h
+++ .config/st/luke-st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "SauceCodePro Nerd Font:pixelsize=12:antialias=true:autohint=true";
+static char *font = "SauceCodePro NF:pixelsize=12:antialias=true:autohint=true";
 static char *font2[] = { "Noto Color Emoji:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
