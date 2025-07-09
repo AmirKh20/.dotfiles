bdcb371bd8fefa734049b8fda6b305d13a1d134d
amir <amirkh20@protonmail.com>
2025-07-09 22:44:16 +0330

    changes for dual monitor set with one being named eDP-1 on the right side and another DP-2 on the left with resolution 2560x1440

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -697,11 +697,10 @@ client.connect_signal("manage", function (c)
     end
 end)
 
---[[ Enable sloppy focus, so that focus follows mouse.
---client.connect_signal("mouse::enter", function(c)
---     c:emit_signal("request::activate", "mouse_enter", {raise = true}) --was false before
---end)
-]]
+-- Enable sloppy focus, so that focus follows mouse.
+client.connect_signal("mouse::enter", function(c)
+     c:emit_signal("request::activate", "mouse_enter", {raise = true}) --was false before
+end)
 
 -- No border for maximized clients
 function border_adjust(c)
